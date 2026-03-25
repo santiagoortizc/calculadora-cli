4995c751d963dc6f19670f7ef05064912da7b218
Santiago Ortiz <santiago.ortizc15@gmail.com>
2026-03-25 16:30:25 -0500

    Agregadas las operaciones de (%) módulo y (**) potencia

--- calculadora.py
+++ calculadora.py
@@ -29,11 +29,23 @@ def dividir(a: float, b: float) -> float:
     return a / b
 
 
+def modulo(a: float, b: float) -> float:
+    if b == 0:
+        raise ValueError("No se puede dividir entre cero.")
+    return a % b
+
+
+def potencia(a: float, b: float) -> float:
+    return a**b
+
+
 OPERACIONES = {
     "+": sumar,
     "-": restar,
     "*": multiplicar,
     "/": dividir,
+    "%": modulo,
+    "**": potencia,
 }
 
 
